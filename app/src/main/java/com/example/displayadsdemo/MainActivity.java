804cb04f05c4cefa1022bd3123c8f5baf2fb764f
Raunak Chandak <raunak.chandak@zee.com>
2023-02-09 17:16:01 +0530

    initial commit

---
+++ app/src/main/java/com/example/displayadsdemo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.displayadsdemo;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.admanager.AdManagerAdRequest;
+import com.google.android.gms.ads.admanager.AdManagerAdView;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends FragmentActivity {
+    private AdManagerAdView mAdManagerAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        WebView web_view = (WebView) findViewById(R.id.webview);
+        web_view.getSettings().setJavaScriptEnabled(true);
+        web_view.loadUrl("file:///android_asset/index.html");
+
+
+    }
+}
